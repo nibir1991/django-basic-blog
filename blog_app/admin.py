57189ed7f8aa938bef1a8fc8e23b2bccb61e11a3
Rayhan Arafat Nibir <nibir.rayhan1991@gmail.com>
2020-11-16 07:55:08 +0600

    Add files via upload

---
+++ blog_app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import post,category,profile,comment
+
+# Register your models here.
+admin.site.register(post)
+admin.site.register(category)
+admin.site.register(profile)
+admin.site.register(comment)
